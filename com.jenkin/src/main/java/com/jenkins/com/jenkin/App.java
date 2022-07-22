5aec90f1253f4ece4b090cc13b684895eeb093e9
apmosys <apmosys@APMOSYSLT0170>
2022-07-22 18:09:46 +0530

    this build is done

---
+++ com.jenkin/src/main/java/com/jenkins/com/jenkin/App.java
@@ -0,0 +1,12 @@
+package com.jenkins.com.jenkin;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class App {
+    public static void main(String[] args) {
+       
+        System.out.println("hello");
+    }
+}
